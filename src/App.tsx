e4bb6c8860add085194403b7991b561eb45146cd
taylanhubeylioglu <taylanhubeylioglu@gmail.com>
2023-08-07 04:30:46 +0300

    data created

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(){
+  return(
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
